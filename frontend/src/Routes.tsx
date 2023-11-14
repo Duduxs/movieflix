6f2ed9dc8b366375984cbdafda92bc260ba0da99
Eduardo José <duduxss3@gmail.com>
2023-11-14 10:48:29 -0300

    Page home done

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,18 +1,48 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import { Redirect, Route, Router, Switch } from "react-router-dom";
 import Navbar from "./core/components/Navbar";
+import history from "./core/utils/history";
 import Home from "./pages/Home";
+import CreateAccount from "./pages/Home/CreateAccount";
+import Movies from "./pages/Movies";
+import PrivateRoute from "./core/components/Routes/PrivateRoute";
+import { isAuthenticated } from "./core/utils/auth";
+import MovieDetails from "./pages/Movies/components/MovieDetails";
 
 const Routes = () => (
-  <BrowserRouter>
+  <Router history={ history }>
     <Navbar />
 
     <Switch>
-      <Route path="/" exact>
-        <Home />
-      </Route>
+      <Redirect from="/" to="/login" exact />
+      <Route
+        path="/login"
+        render={() => {
+          return (isAuthenticated() ?
+            <Redirect to='/movies' /> :
+            <Home />
+          )
+        }}
+      />
+
+      <Route
+        path="/criar-conta"
+        render={() => {
+          return (isAuthenticated() ?
+            <Redirect to='/movies' /> :
+            <CreateAccount />
+          )
+        }}
+      />
+
+      <PrivateRoute path="/movies" exact>
+        <Movies />
+      </PrivateRoute>
+
+      <PrivateRoute path="/movies/:movieId">
+        <MovieDetails />
+      </PrivateRoute>
     </Switch>
-  </BrowserRouter>
+  </Router>
 )
 
 export default Routes
--- frontend/src/core/components/Navbar/index.tsx
+++ frontend/src/core/components/Navbar/index.tsx
@@ -1,17 +1,31 @@
-import { Link } from 'react-router-dom'
-
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { isAuthenticated, logout } from '../../utils/auth'
 import './styles.scss'
 
 const Navbar = () => {
+  const [isUserLogged, setIsUserLogged] = useState({})
+  const location = useLocation()
+
+  useEffect(() => {
+    const userLogged = isAuthenticated()
+    setIsUserLogged(userLogged)
+  }, [location])
+
   return (
     <nav className="navbar-container">
       <div className="navbar-content">
-        <Link to="/" className="navbar-logo">MovieFlix</Link>
-        <div className="navbar-logout-button">
-          <span className="navbar-logout-button-text">
-            Sair
-          </span>
-        </div>
+        <Link to="/movies" className="navbar-logo">MovieFlix</Link>
+        {isUserLogged && (
+          <div
+            className="navbar-logout-button"
+            onClick={ logout }
+          >
+            <span className="navbar-logout-button-text">
+              Sair
+            </span>
+          </div>
+        )}
       </div>
     </nav>
   )
