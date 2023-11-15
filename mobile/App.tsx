2ee6d6eb23ba31b378342044a343dbafe614555a
Eduardo José <duduxss3@gmail.com>
2023-11-15 18:32:21 -0300

    Setting up the rest of the components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { useFonts, OpenSans_400Regular, OpenSans_700Bold } from '@expo-google-fonts/open-sans'
+import AppLoading from 'expo-app-loading'
+
+import Routes from './src/routes';
+import { AuthContext } from './src/contexts/AuthContext';
+import { isAuthenticated } from './src/core/utils/auth';
 
 export default function App() {
+  const [isUserLogged, setIsUserLogged] = useState(setUserLogged() ? true : false)
+
+  async function setUserLogged() {
+    const isUserAuthenticated = await isAuthenticated()
+
+    if (isUserAuthenticated)
+      setIsUserLogged(true)
+    else
+      setIsUserLogged(false)
+  }
+
+  const [ fontsLoaded ] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_700Bold
+  })
+
+  if(!fontsLoaded)
+    return <AppLoading />
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthContext.Provider value={{ isUserLogged, setUserLogged }}>
+        <Routes />
+      </AuthContext.Provider>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-})
